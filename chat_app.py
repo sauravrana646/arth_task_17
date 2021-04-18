23d32efb22f64bd34de46533a77f0f2f4d8947c4
sauravrana646 <42442467+sauravrana646@users.noreply.github.com>
2021-04-18 20:52:39 +0530

    Update chat_app.py

--- chat_app.py
+++ chat_app.py
@@ -4,7 +4,7 @@ import threading
 
 def rcv():
     r = socket.socket(family=socket.AF_INET , type=socket.SOCK_DGRAM)
-    r.bind(("__ip_here__" , __port_here___))
+    r.bind(("__your_ip_here__" , __your_port_here___))
     while True : 
         rd = r.recvfrom(4096)
         print(f"\n{rd[1][0]:>{90}s} : {rd[0].decode()}")      # f"{py:>{width}s} : {vg:>{width}s}"
@@ -13,7 +13,7 @@ def snd():
     s = socket.socket(family=socket.AF_INET , type=socket.SOCK_DGRAM)
     while True:
         data = input()
-        s.sendto(data.encode(), ("__ip_here__" , __port_here___))
+        s.sendto(data.encode(), ("__target_ip_here__" , __target_port_here___))
         input()
        
 x = threading.Thread(target=snd)
